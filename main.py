783d78ba06e282d2f14ab8ee2631b2dd0ef9dc6a
s175012 <147266485+s175012@users.noreply.github.com>
2024-02-25 14:55:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+
+print(sys.version)
+
+#file-settings-version control-github-add-zaloguj się w przeglądarce
+#git config --list
+#git config --global user.name s175012
+#git config --global user.email 147266485+s175012@users.noreply.github.com

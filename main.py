768ebccf0be25850b28d09efa737b5d3fa341696
s175012 <147266485+s175012@users.noreply.github.com>
2024-02-25 15:05:53 +0100

    + plik lab1

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print(sys.version)
 #git config --list
 #git config --global user.name s175012
 #git config --global user.email 147266485+s175012@users.noreply.github.com
+#vcs-share on github
+
+a = 5
+print(a)
+
+#git-commit
